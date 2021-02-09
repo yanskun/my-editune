c755de83d32f6566b9a18b09933fc13cdeb35afc
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-02-10 01:55:38 +0900

    feat: select story

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,7 +4,7 @@ module.exports = {
     {
       name: '@storybook/addon-essentials',
       options: {
-        actions: false,
+        actions: true,
         backgrounds: false,
         controls: true,
         docs: false,
@@ -12,5 +12,25 @@ module.exports = {
         toolbars: false,
       },
     },
+    '@storybook/addon-knobs',
   ],
+  webpackFinal: async (config, { configType }) => {
+    // `configType` has a value of 'DEVELOPMENT' or 'PRODUCTION'
+    // You can change the configuration based on that.
+    // 'PRODUCTION' is used when building the static version of storybook.
+
+    // Make whatever fine-grained changes you need
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: ['style-loader', 'css-loader', 'sass-loader'],
+    })
+
+    config.module.rules.push({
+      test: /\.pug$/,
+      use: [{ loader: 'pug-plain-loader' }],
+    })
+
+    // Return the altered config
+    return config
+  },
 }
