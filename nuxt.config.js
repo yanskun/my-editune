48fc335fc0eeba6d68ef7caf9ae9cf7528563957
yasudanaoya <43776161+yasudanaoya@users.noreply.github.com>
2021-05-29 03:59:41 +0900

    fix: nuxt config

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,10 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["element-ui/lib/theme-chalk/index.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: ["@/plugins/element-ui"],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
